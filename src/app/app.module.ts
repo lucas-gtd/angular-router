5c842a57bc4cc336b2ed1725bedd2b269884e1da
lucas-gtd <lucasgoutaudier@gmail.com>
2021-04-13 08:26:00 +0200

    quete router terminee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router'
+import { ContactComponent } from './contact/contact.component'
+import { HomeComponent } from './home/home.component'
+
+export const ROUTES: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
